9f5e6159538fabb52310f7ce703c0aa979f94130
Andres Santamarina <andresesantamarina@gmail.com>
2023-11-14 15:49:45 -0300

    ejercicio 1 del practico 4 resuelto

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,11 +1,23 @@
 /*1- Crea un objeto llamado auto que tenga algunas características como el color, marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.
 
 Output:
-
-
-
 objeto.encender();
 objeto.apadar()
 auto encendido
 El auto se apagó
 */
+
+const auto = {
+    color: 'rojo',
+    marca: 'BMW',
+    modelo: 'Sedán',
+    encender: function(){
+        document.write('<p>Auto encendido.</p>')
+    },
+    apagar: ()=>{
+        document.write('<p>El auto se apagó.</p>')
+    }
+}
+
+auto.encender();
+auto.apagar();
