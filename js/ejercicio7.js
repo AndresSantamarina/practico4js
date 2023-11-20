3963ceefc66287cfd58f7e2b14555f03646910e3
Andres Santamarina <andresesantamarina@gmail.com>
2023-11-20 12:59:37 -0300

    ejercicio 7 del practico 4 sin terminar, estan creadas las clases contacto y agenda

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -17,3 +17,90 @@ huecosLibres(): indica cuántos contactos más podemos ingresar.
 
 Crea un menú con opciones que serán seleccionadas por el usuario usando un prompt, las salidas de las operaciones seleccionadas por el usuario se pueden mostrar en pantalla y  por consola.
 */
+
+class Contacto {
+    constructor(nombre, telefono){
+        this._nombre = nombre;
+        this._telefono = telefono;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+    get telefono(){
+        return this._telefono;
+    }
+    set nombre(nuevoNombre){
+        this._nombre = nuevoNombre;
+    }
+    set telefono(nuevoTelefono){
+        this._telefono = nuevoTelefono;
+    }
+}
+
+class Agenda {
+    constructor(tamanio = 10){
+        this._contactos = [];
+        this._tamanioMaximo = tamanio;
+    }
+
+    get contactos(){
+        return this._contactos;
+    }
+    get tamanioMaximo(){
+        return this._tamanioMaximo;
+    }
+    set contactos(nuevoContactos){
+        this._contactos = nuevoContactos;
+    }
+    set tamanioMaximo(nuevoTamanioMaximo){
+        this._tamanioMaximo = nuevoTamanioMaximo;
+    }
+
+    aniadirContacto(contacto){
+        this.contactos.push(contacto);
+    }
+
+    existeContacto(contacto){
+        if(this.contactos.nombre === contacto.nombre){
+            alert('El contacto ya existe en la agenda')
+        }else{
+            alert('El contacto no existe')
+        }
+    }
+
+    listarContactos(){
+        for ( let i=0; i<this.contactos.length;i++){
+            document.write(`<ul>
+            <li>Nombre: ${this.contactos}</li>
+            </ul>`)
+        }
+    }
+
+    buscarContacto(nombre){
+
+    }
+
+    eliminarContacto(contacto){
+
+    }
+
+    agendaLlena(){
+        if(this.contactos.length >= this.tamanioMaximo){
+            alert('La agenda está llena')
+        }else{
+            alert('La agenda tiene espacio')
+        }
+    }
+
+    huecosLibres(){
+        const espacioLibre = this.tamanioMaximo - this.contactos.length;
+        alert(`Queda/n ${espacioLibre} espacio/s libre/s`)
+    }
+}
+
+const agenda = new Agenda()
+
+agenda.aniadirContacto(new Contacto('Andrés',123456789));
+console.log(agenda.contactos)
+agenda.listarContactos()
